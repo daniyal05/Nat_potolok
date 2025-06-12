650f3f9af4a7dfd2b771f4f3d8e71ef95db5fb81
Daniyal <dkurbanov2009@gmail.com>
2025-06-12 15:48:46 +0300

    add swiper

---
+++ script.js
@@ -0,0 +1,20 @@
+new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  // Pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 3000,
+
+  }
+});
